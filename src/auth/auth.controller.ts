56b4afe0154fa1f411148be7368c9587dffaeb40
namphan2599 <namphan2599@gmail.com>
2023-04-25 21:51:09 +0700

    add guard for authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,36 @@
-import { Controller, Post, Body} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Request,
+  UseGuards,
+	UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import AuthUserLoginDto from './dto/auth-user-login';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
 
-    constructor(private authService: AuthService) {}
+  @Post('/signIn')
+  async signIn(@Body() bd: AuthUserLoginDto) {
+    const rs = await this.authService.signIn(bd.username, bd.password);
 
-    @Post('/signIn')
-    async signIn(@Body() bd: AuthUserLoginDto) {
-        const rs = await this.authService.signIn(bd.username, bd.password);
-        
-        return rs;
-    }
-    
+    return rs;
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('testJwt')
+  test(@Request() req) {
+    if (req.user) {
+      return {
+        status: 'ok',
+      };
+    } else {
+			throw new UnauthorizedException();
+		}
+  }
 }
